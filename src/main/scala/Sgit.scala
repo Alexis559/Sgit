5683bfe26f14206dc1bbb27a82dc5a82ccd8f921
Alexis Sanchez <alexis559.sanchez@gmail.com>
2019-10-17 22:54:48 +0200

    Added Branch & checkout on Branch.

--- src/main/scala/Sgit.scala
+++ src/main/scala/Sgit.scala
@@ -1,4 +1,3 @@
-import core.Test
 import core.commands._
 import core.objects.Checkout
 import utils.io.IO
@@ -11,10 +10,10 @@ object Sgit extends App {
       case "init" => InitCmd.init(IO.getCurrentPath)
       case "add" => AddCmd.add(config.filesAdd)
       case "commit" => CommitCmd.commit(config.commitName)
-      case "status" => Test.time(StatusCmd.status())
+      case "status" => StatusCmd.status()
       case "tag" => TagCmd.tag(config.tagName)
       case "diff" => DiffCmd.diff()
-      case "branch" => Test.time(if (config.verboseBranch) BranchCmd.branchList() else BranchCmd.branch(config.branchName))
+      case "branch" => if (config.verboseBranch) BranchCmd.branchList() else BranchCmd.branch(config.branchName)
       case "checkout" => CheckoutCmd.checkout(config.branchName)
       case "test" => Printer.displayln(Checkout.recreateWorkingDirectory("test").toString) // TODO DELETE THIS AT THE END
       case _ => Printer.displayln(config.toString)
